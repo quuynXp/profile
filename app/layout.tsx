a3f309ece9d849ed6ebbe140c949c1d5a945bd17
Quyen Ngoc <quyen10924@gmail.com>
2025-07-23 19:06:19 +0700

    update v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,34 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Thai Ngoc Quyen',
-  description: 'NgocQuyen Pofolio',
+  description: 'NgocQuyen Portfolio',
   generator: 'Thai Ngoc Quyen',
+  icons: {
+    icon: '/favicon.png',
+  },
+  openGraph: {
+    title: 'Thai Ngoc Quyen',
+    description: 'Check out my portfolio!',
+    url: 'https://thaingocquyen.vercel.app',
+    siteName: 'Thai Ngoc Quyen',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Thai Ngoc Quyen Portfolio',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Thai Ngoc Quyen',
+    description: 'Check out my portfolio!',
+    images: ['/og-image.png'],
+  },
 }
+
 
 export default function RootLayout({
   children,
